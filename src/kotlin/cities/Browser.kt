63949563a98d489b877533cca7aaded6797ca4e6
stormkirill98 <stormkirill98@gmail.com>
2020-03-02 22:16:12 +0300

    clear project

--- src/kotlin/cities/Browser.kt
+++ src/kotlin/cities/Browser.kt
@@ -1,109 +1,11 @@
-import org.json.JSONArray
-import org.json.JSONObject
+package cities
+
 import org.openqa.selenium.*
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.interactions.Actions
 import java.io.Closeable
-import java.io.File
 import java.util.concurrent.TimeUnit
-
-const val FILE_NAME = "more_cities"
-const val CIAN_URL = "https://yaroslavl.cian.ru/"
-const val AVITO_URL = "https://www.avito.ru/yaroslavl"
-
-const val CIAN_BUTTON_SHOW_SEARCH_CLASS = "c-header-top-link c-header-top-menu-item"
-const val CIAN_SEARCH_INPUT_CLASS =
-    "text_field_component-input-r5g2rpzU text_field_component-small-8egraV0q input--9c1e16c1ec4b69df81049cd40d4c9ed4"
-const val CIAN_SUGGESTIONS_LIST_CLASS = "list--79e175a2c2de90a657da1f79e3a62c08"
-const val CIAN_ENTER_BUTTON =
-    "button_component-button-3KmZZLvJ button_component-S-th694M5g button_component-default-LU0A0kvh button_component-primary-2nYBoa2Y button--8da9b20b88751e792a7fc2fcb7bd90ee"
-const val CIAN_CLOSE_SEARCH_CLASS = "close--b220039b0345a01d127bf4b34008b665"
-
-const val AVITO_BUTTON_SHOW_SEARCH_CLASS = "main-locationWrapper-3C0pT"
-const val AVITO_SEARCH_INPUT_CLASS = "suggest-input-3p8yi"
-const val AVITO_SUGGESTIONS_LIST_CLASS = "suggest-suggests-bMAdj"
-const val AVITO_ENTER_BUTTON = "button-button-2Fo5k button-size-m-7jtw4 button-primary-1RhOG"
-const val AVITO_CLOSE_SEARCH_CLASS = "popup-close-2W0cr"
-
-enum class Cite { AVITO, CIAN }
-
-fun main() {
-    Logger.logNewRunning()
-
-    val cities = readCities()
-    val jsonArray = JSONArray()
-
-    for ((i, cityName) in cities.withIndex()) {
-        println("Read ${i + 1}. $cityName")
-
-        val jsonCity = JSONObject().apply {
-            put("id", i + 1)
-            put("name", cityName)
-        }
-
-        jsonArray.put(jsonCity)
-    }
-
-    Browser().use {
-        it.openCianPage()
-        fillCities(Cite.CIAN, jsonArray, it)
-
-        it.openAvitoPage()
-        fillCities(Cite.AVITO, jsonArray, it)
-    }
-
-
-    val filteredArray =
-        jsonArray.filter {
-            !((it as JSONObject).getString("cian_url").isEmpty()
-                    && it.getString("cian_url").isEmpty())
-        }
-
-    println("Save ${filteredArray.size}")
-
-    writeCities(JSONArray(filteredArray))
-}
-
-fun fillCities(cite: Cite, jsonArray: JSONArray, browser: Browser) {
-    for ((i, jsonObj) in jsonArray.withIndex()) {
-        if (jsonObj !is JSONObject) continue
-
-        val name = jsonObj.getString("name")
-        val percent = (i + 1) / jsonArray.length().toDouble()
-        println("$i Fill $cite. $name       ${percent.formatExt(2)}")
-
-        val (url, id) = if (cite == Cite.AVITO)
-            browser.getAvitoInfo(name)
-        else browser.getCianInfo(name)
-
-        if (url.isEmpty() || id == null) {
-            Logger.logNotDefineUrlOrId(name, cite.name)
-        }
-
-        jsonObj.put(cite.name.toLowerCase() + "_url", url)
-        jsonObj.put(cite.name.toLowerCase() + "_id", id)
-        Thread.sleep(500)
-    }
-}
-
-fun readCities(): List<String> {
-    val file = {}.javaClass.getResource("$FILE_NAME.csv")
-    val fileText = file.readText()
-
-    return fileText.lines().map { it.substringAfter(",") }
-}
-
-fun writeCities(jsonArray: JSONArray) {
-    val path = System.getProperty("user.dir") + "\\src\\resources\\$FILE_NAME.json"
-    val jsonFile = File(path)
-    jsonFile.createNewFile()
-
-    val jsonObject = JSONObject()
-    jsonObject.put("cities", jsonArray)
-
-    jsonFile.appendText(jsonObject.toString(2))
-}
 
 class Browser : Closeable {
     private val driver: WebDriver
@@ -252,5 +154,3 @@ class Browser : Closeable {
         Thread.sleep(500)
     }
 }
-
-fun Double.formatExt(digits: Int) = "%.${digits}f".format(this)
---
+++ src/kotlin/cities/CheckCities.kt
@@ -0,0 +1,25 @@
+package cities
+
+import org.json.JSONObject
+
+fun main() {
+    val file = {}.javaClass.getResource("parse_cities.json")
+    val fileText = file.readText()
+
+    val jsonArray = JSONObject(fileText).getJSONArray("cities")
+
+    val filteredArray = jsonArray.filter { (it as JSONObject).getString("cian_url").isEmpty()
+            || it.getString("avito_url").isEmpty() }
+
+    val countCianEmpty = filteredArray.count { (it as JSONObject).getString("cian_url").isEmpty() }
+    val countAvitoEmpty = filteredArray.count { (it as JSONObject).getString("avito_url").isEmpty() }
+
+    println("Count cian city not define: $countCianEmpty")
+    println("Count avito city not define: $countAvitoEmpty")
+
+    val avitoNotDefineCities = filteredArray
+        .filter { (it as JSONObject).getString("avito_url").isEmpty() }
+        .map { (it as JSONObject).getString("name") }
+
+    avitoNotDefineCities.forEach(::println)
+}
